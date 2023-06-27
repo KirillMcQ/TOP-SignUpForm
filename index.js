812a1c688dfc6a834f42108aa675717b74b3c314
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-06-27 18:23:58 -0400

    Finished form validations

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // File is mainly for validations of input
+
+let formSubmitBtn = document.querySelector(".formSubmit");
+let form = document.querySelector("#inputs-container");
+let inputEls = document.getElementsByTagName("input");
+let password = document.querySelector("#password");
+let confirmPassword = document.querySelector("#confirm_password");
+let passwordMatchErr = document.querySelector(".passwordMatchErr");
+
+
+formSubmitBtn.addEventListener("click", () => {
+    let willSubmit = true;
+    // Validate inputs one by one
+    Array.from(inputEls).forEach((inputEl) => {
+        if ((inputEl.classList[0] === "required" && inputEl.value === "")) {
+            willSubmit = false;
+            inputEl.style.border = "1px solid red";
+        };
+        if (!inputEl.checkValidity()) {
+            inputEl.reportValidity();
+            willSubmit = false;
+        }
+    });
+    // Check if confirm password matches password
+    if (password.value !== confirmPassword.value) {
+        passwordMatchErr.style.display = "block";
+        willSubmit=false
+    };
+
+    if (willSubmit) form.submit();
+    else console.log("Not Submiting");
+});
